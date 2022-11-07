dfff605055b2e78b5a514495236b84eaebb20710
SaiaIlham <ilhamindra09@mhs.mdp.ac.id>
2022-11-07 14:25:17 +0700

    Awalan

---
+++ app/src/main/java/putri/if3b/uts/kulinerkita/MainActivity.java
@@ -0,0 +1,14 @@
+package putri.if3b.uts.kulinerkita;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
